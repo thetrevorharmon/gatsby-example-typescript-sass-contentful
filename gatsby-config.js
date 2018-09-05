9f02ed71bb593c411dd08f2cb2a02f49da0d2453
Trevor Harmon <trevor.harmon@parivedasolutions.com>
2018-09-05 14:39:30 -0700

    Integrated contentful, displaying movie titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
+require("dotenv").config({path: `.env`,})
+  
 module.exports = {
   siteMetadata: {
-    name: `Hello Typescript World!`,
-    tagline: `Gatsby + SASS + Typescript = 💪`
+    name: `Star Wars Movies 🎥`,
+    tagline: `"Do, or do not. There is no try."`
   },  
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+      },
+    },
   ],
 }
